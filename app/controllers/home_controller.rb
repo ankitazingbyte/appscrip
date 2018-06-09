f2bba0d3148a48e708c3ef88e03f97aa83bba1d1
ankita dangi <ankita.dangi@zingbytes.com>
2018-06-09 11:18:31 +0530

    how it work contact

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,20 +2,25 @@ class HomeController < ApplicationController
 	
 	def index
 		@demands = Demand.all
+		@contact = Contact.new
 	end
 
 	def faq
 		@faqs = Faq.all
+		@contact = Contact.new
 	end
 
 	def about
 		@teams = Team.all
+		@contact = Contact.new
 	end
 
 	def landing
+		@contact = Contact.new
 	end
 	
 	def howitwork
+		@contact = Contact.new
 	end
 
 end
